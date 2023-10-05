5c4e208e644a89ecd8c57340c529561c7dbf4b45
Edson <edisaiassan@gmail.com>
2023-10-05 14:31:46 -0500

    conectando a la base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,21 @@
-/* import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
-
-@Module({
-  imports: [CatsModule],
-  controllers: [AppController],
-  providers: [AppService],
-})
-export class AppModule {}
- */
-
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3307,
+      username: 'user_crud',
+      password: 'root',
+      database: 'db_crud',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,15 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { UpdateCatDto } from './dto/update-cat.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Cat } from './entities/cat.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class CatsService {
-  create(createCatDto: CreateCatDto) {
+
+  constructor(
+    @InjectRepository(Cat)
+  private readonly catRepository: Repository<Cat>,
+  ) {
+
+  }
+  
+  async create(createCatDto: CreateCatDto) {
+
     return 'This action adds a new cat';
   }
 
-  findAll() {
-    return `This action returns all cats`;
+  async findAll() {
+    return await this.catRepository.find();
   }
 
   findOne(id: number) {
