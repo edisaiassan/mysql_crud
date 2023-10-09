c87f390accc2344bd418570230253945de999020
Edson <edisaiassan@gmail.com>
2023-10-09 09:51:57 -0500

    update

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -3,11 +3,12 @@ import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Cat } from './entities/cat.entity';
-import { Breed } from 'src/breeds/entities/breed.entity';
+import { BreedsModule } from 'src/breeds/breeds.module';
+import { BreedsService } from 'src/breeds/breeds.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cat, Breed])],
+  imports: [TypeOrmModule.forFeature([Cat, BreedsModule])],
   controllers: [CatsController],
-  providers: [CatsService],
+  providers: [CatsService, BreedsService],
 })
 export class CatsModule {}  
